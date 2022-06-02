46bacb1edd518939df7f9193fba7d5e0f477e726
98ping <67809373+98ping@users.noreply.github.com>
2022-06-01 21:15:01 -0700

    massive overhaul of the entire application

--- manager/src/main/kotlin/ltd/matrixstudios/framework/FrameworkManager.kt
+++ manager/src/main/kotlin/ltd/matrixstudios/framework/FrameworkManager.kt
@@ -1,26 +1,27 @@
 package ltd.matrixstudios.framework
 
 import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.api.command.CreateContainerResponse
+import com.github.dockerjava.api.exception.InternalServerErrorException
+import com.github.dockerjava.api.model.ExposedPort
+import com.github.dockerjava.api.model.Ports
 import com.github.dockerjava.core.DefaultDockerClientConfig
 import com.github.dockerjava.core.DockerClientBuilder
-import com.github.dockerjava.core.DockerClientConfig
 import ltd.matrixstudios.framework.configs.Config
 import ltd.matrixstudios.framework.tasks.CheckServersAndGames
-import kotlin.concurrent.thread
+import java.util.*
+
 
 class FrameworkManager {
 
-    var dockerClient: DockerClient
 
     init {
         Config.load()
 
-        val dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder().withDockerHost("tcp://172.18.0.1:2375").build()
-        val dockerClient = DockerClientBuilder.getInstance(dockerClientConfig).build()
-
-        this.dockerClient = dockerClient
+        Runtime.getRuntime().exec("screen -dmS na-sg-1")
 
         FrameworkShared.startup(Config.mongoURI!!, Config.redisURI!!)
+
 
         CheckServersAndGames().start()
 
--- manager/src/main/kotlin/ltd/matrixstudios/framework/tasks/CheckServersAndGames.kt
+++ manager/src/main/kotlin/ltd/matrixstudios/framework/tasks/CheckServersAndGames.kt
@@ -11,28 +11,33 @@ import java.util.concurrent.TimeUnit
 class CheckServersAndGames : Thread() {
 
     override fun run() {
-        FrameworkManager.instance.dockerClient.createContainerCmd("na-sg-1")
-        for (server in GameServerService.findAll()) {
-            if (System.currentTimeMillis() - server.lastUpdated >= TimeUnit.MINUTES.toMillis(1))
-            {
-                server.lastUpdated = 0L
-                server.online = false
-                server.currentlyFull = false
-                server.currentlyPlaying.clear()
+        while (true) {
+            for (server in GameServerService.findAll()) {
+                if (System.currentTimeMillis() - server.lastUpdated >= TimeUnit.MINUTES.toMillis(1)) {
+                    server.lastUpdated = 0L
+                    server.online = false
+                    server.currentlyFull = false
+                    server.currentlyPlaying.clear()
 
-                println("[Monitor] Server ${server.id} was found to be unresponsive. Closed the server from the database end")
-                GameServerService.save(server.id, server)
-            }
-        }
-
-        for (game in GameService.findAll()) {
-            if (System.currentTimeMillis() - game.lastUpdated >= TimeUnit.SECONDS.toMillis(30))
-            {
-                RedisDetails.jedisResource.use {
-                    it.hdel("Framework:games:", game.id)
+                    println("[Monitor] Server ${server.id} was found to be unresponsive. Closed the server from the database end")
+                    GameServerService.save(server.id, server)
                 }
+            }
 
-                println("[Monitor] Game ${game.id} was found to be unresponsive. Deleted it from the values")
+            for (game in GameService.findAll()) {
+                if (System.currentTimeMillis() - game.lastUpdated >= TimeUnit.SECONDS.toMillis(30)) {
+                    RedisDetails.jedisResource.use {
+                        it.hdel("Framework:games:", game.id)
+                    }
+
+                    println("[Monitor] Game ${game.id} was found to be unresponsive. Deleted it from the values")
+                }
+            }
+
+            try {
+                sleep(1500L)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
             }
         }
     }
