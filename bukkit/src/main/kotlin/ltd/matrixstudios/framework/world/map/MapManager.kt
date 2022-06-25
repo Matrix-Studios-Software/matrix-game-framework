fe5ba129ebfa3b794af4a68fc861226e1ed0b461
98ping <67809373+98ping@users.noreply.github.com>
2022-06-25 10:41:50 -0700

    add cuboid support and start to work on the actual map manager features

--- bukkit/src/main/kotlin/ltd/matrixstudios/framework/world/map/MapManager.kt
+++ bukkit/src/main/kotlin/ltd/matrixstudios/framework/world/map/MapManager.kt
@@ -1,6 +1,8 @@
 package ltd.matrixstudios.framework.world.map
 
 import ltd.matrixstudios.framework.data.mongo.MongoRepository
+import org.bukkit.Chunk
+import org.bukkit.ChunkSnapshot
 
 object MapManager : MongoRepository<String, Map>(
     "maps",
